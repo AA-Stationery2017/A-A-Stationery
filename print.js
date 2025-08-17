4b169f73447647ac6da73c96401591c58e34e9d3
AA-Stationery2017 <aastationary2017@gmail.com>
2025-08-17 17:32:39 +0500

    Update print.js

--- print.js
+++ print.js
@@ -1,8 +1,20 @@
 const printItems = [
-    { name: "Color Print (A4)", image: "color-print.jpg", price: "15" },
-    { name: "Black & White Print (A4)", image: "bw-print.jpg", price: "5" },
-    { name: "Lamination", image: "lamination.jpg", price: "20" },
-    { name: "Photo Print", image: "photo-print.jpg", price: "25" }
+    { name: "Color Print (A4)",
+     image: "color-print.jpg",
+     price: "15"
+    },
+    { name: "Black & White Print (A4)", 
+     image: "bw-print.jpg",
+     price: "5"
+    },
+    { name: "Lamination",
+     image: "lamination.jpg",
+     price: "20" 
+    },
+    { name: "Photo Print",
+     image: "photo-print.jpg",
+     price: "25"
+    }
 ];
 
 const printList = document.getElementById('printList');
@@ -17,21 +29,6 @@ printItems.forEach((item, idx) => {
             <label>Qty:</label>
             <input type="number" min="1" value="1" name="print-qty-${idx}">
         </div>
-        <button class="add-to-cart" data-idx="${idx}">Add to Cart</button>
     `;
     printList.appendChild(card);
-});
-printList.addEventListener('click', function(e) {
-    if (e.target.classList.contains('add-to-cart')) {
-        const idx = e.target.getAttribute('data-idx');
-        const qty = parseInt(document.getElementById(`print-qty-${idx}`).value, 10);
-        const item = printItems[idx];
-        addToCart({
-            name: item.name,
-            image: item.image,
-            price: Number(item.price),
-            quantity: quantity 
-        });
-        alert(`${item.name} added to cart!`);
-    }
 });
